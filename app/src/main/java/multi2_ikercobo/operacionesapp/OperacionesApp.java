0d2576710bf1a28207ace8120ca0924feefb0ef2
iker cobo <ikercg96_@hotmail.com>
2018-12-05 18:05:39 +0100

    Initial commit

---
+++ app/src/main/java/multi2_ikercobo/operacionesapp/OperacionesApp.java
@@ -0,0 +1,13 @@
+package multi2_ikercobo.operacionesapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OperacionesApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_operaciones_app);
+    }
+}
